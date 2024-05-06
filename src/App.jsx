e5076bdae031c71a9cc4c0043bdc1c47a39686c2
John Waweru <johnwaweru092@gmail.com>
2024-05-06 11:34:43 +0300

    Defined PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ function App() {
   return (
     <div>
       <h1>Hello Kamau</h1>
-      <UserProfile username="wawesh" age={31} email="john@test.com" />
+      <UserProfile
+        username="wawesh"
+        age={31}
+        email="john@test.com"
+        myFunc={() => console.log("Hello World")}
+        myArray={[
+          { id: 1, name: "John" },
+          { id: 2, name: "David" },
+        ]}
+      />
     </div>
   );
 }
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,12 +6,27 @@ const UserProfile = (props) => {
     <div>
       <p>Username: {props.username}</p>
       <p>Email: john@test.com</p>
+      <ul>
+        {props.myArray.map((item, index) => {
+          return <li key={index}>{item.name}</li>;
+        })}
+      </ul>
+      <button type="button" onClick={props.myFunc}>
+        Click Me
+      </button>
     </div>
   );
 };
 
 UserProfile.propTypes = {
   username: PropTypes.string.isRequired,
+  myFunc: PropTypes.func,
+  myArray: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default UserProfile;
